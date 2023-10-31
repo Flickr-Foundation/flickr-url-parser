0b11a4d5d7e9e7132a42c59f3602d299f98efc2f
Alex Chan <alex@alexwlchan.net>
2023-10-31 11:38:05 +0000

    Fix coverage on python 3.7

--- src/flickr_url_parser/types.py
+++ src/flickr_url_parser/types.py
@@ -3,7 +3,7 @@ from typing import Union
 
 # See https://mypy.readthedocs.io/en/stable/runtime_troubles.html#using-new-additions-to-the-typing-module
 # See https://github.com/python/mypy/issues/8520
-if sys.version_info >= (3, 8):
+if sys.version_info >= (3, 8):  # pragma: no cover
     from typing import Literal, TypedDict
 else:  # pragma: no cover
     from typing_extensions import Literal, TypedDict
